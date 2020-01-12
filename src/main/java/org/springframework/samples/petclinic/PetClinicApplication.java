44daf9ad9f6d12b9fb364e6d2b45fc579c9133f1
Leonid <pilleo19@gmail.com>
2020-01-12 17:35:39 +0300

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,7 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-        return;
+        new  java.io.FileInputStream("");
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
